994d65df8fde46c24589ad08d4a3c518f3e14374
Paul_Aroo <timmypaul2006@gmail.com>
2022-08-08 18:09:41 +0100

    fix: react router base URL

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Login from "./pages/Login/Login";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/anyRec-React">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
